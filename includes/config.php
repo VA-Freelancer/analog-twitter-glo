407c7bb56ac1bcd2ee614f53bdb430e6dba86636
Vikulov Andrey <vikulov.prog@gmail.com>
2021-11-13 01:00:12 +0400

    Первый шаги(php) Получены данные из базы mysql, Распарсены в твиты Создан переход по ссылке на автора и отборки только его твитов файл раскидан по частям

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+
+    define('SITE_NAME', 'Twitter');
+    define('HOST', 'http://' . $_SERVER['HTTP_HOST']);
+    define('DB_HOST', '127.0.0.1');
+    define('DB_NAME', 'db_twitter');
+    define('DB_CHARSET', 'utf8');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
